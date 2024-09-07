c3b6145012edc3ba920cb70afe5b3e72f39d4ba9
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-09-07 20:52:34 -0300

    feat: Create project

---
+++ src/main/java/com/rogeriogregorio/environmental_reporting_portal/EnvironmentalReportingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.environmental_reporting_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentalReportingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentalReportingPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogeriogregorio/environmental_reporting_portal/EnvironmentalReportingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.environmental_reporting_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentalReportingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
