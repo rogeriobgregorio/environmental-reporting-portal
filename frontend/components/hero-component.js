433313cb340904a17b88eb52160c4fddaed102b3
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-10-06 20:30:43 -0300

    refactor: Refactor frontend components

--- frontend/components/hero-component.js
+++ frontend/components/hero-component.js
@@ -2,9 +2,15 @@ class HeroComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <section id="hero" class="hero">
-        <h1>Seja a Voz da Natureza</h1>
-        <h2>Denuncie agressões ambientais e ajude <br> a propagar ações em defesa do planeta.</h2>
-        <a href="#about" class="cta-btn-report">Quero fazer uma denúncia</a>
+        <div class="hero-content">
+          <h1>Seja a Voz da Natureza</h1>
+          <h2>
+            Denuncie agressões ambientais e ajude a
+            <br> 
+            propagar ações em defesa do planeta.
+          </h2>
+          <a href="#about" class="cta-btn-report">Quero fazer uma denúncia</a>
+        </div>
       </section>
     `;
   }
