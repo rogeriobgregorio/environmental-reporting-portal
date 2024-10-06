5dc04915ba1543473f898df4c5b9ed547d830b0a
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-10-06 18:21:10 -0300

    refactor: Refactor frontend components

--- frontend/components/hero-component.js
+++ frontend/components/hero-component.js
@@ -1,16 +1,12 @@
-import { startCarousel } from "../js/hero-carousel.js";
-
 class HeroComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <section id="hero" class="hero">
         <h1>Seja a Voz da Natureza</h1>
-        <h2>Denuncie agressões ambientais e ajude a propagar ações em defesa do planeta.</h2>
+        <h2>Denuncie agressões ambientais e ajude <br> a propagar ações em defesa do planeta.</h2>
         <a href="#about" class="cta-btn-report">Quero fazer uma denúncia</a>
       </section>
     `;
-
-    startCarousel(this);
   }
 }
 
